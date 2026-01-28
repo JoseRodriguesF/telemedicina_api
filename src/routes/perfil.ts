ff9f048bd668744aa2d03c7629caacb3f7211855
JoseRodriguesF <jose.antonio.220507@gmail.com>
2026-01-28 14:46:32 -0300

    fix: add authentication middleware to profile routes

--- src/routes/perfil.ts
+++ src/routes/perfil.ts
@@ -1,10 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { PerfilController } from '../controllers/perfilController'
+import { authenticateJWT } from '../middlewares/auth'
 
 const perfilController = new PerfilController()
 
 export async function perfilRoutes(app: FastifyInstance) {
-    // Rotas protegidas (o middleware deve ser aplicado globalmente ou injetado aqui)
+    // Todas as rotas de perfil requerem autenticação
+    app.addHook('onRequest', authenticateJWT)
+
     app.get('/usuarios/me', perfilController.getMe.bind(perfilController))
     app.patch('/usuarios/me', perfilController.updateMe.bind(perfilController))
 }
